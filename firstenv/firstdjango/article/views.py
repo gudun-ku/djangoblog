03cfa1d2b3b553e746f82f4e18e3c4527f914761
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-06-04 00:17:57 +0400

    Using Api views

--- firstenv/firstdjango/article/views.py
+++ firstenv/firstdjango/article/views.py
@@ -1,50 +1,19 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from .models import Article
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Article, Author
 from .serializers import ArticleSerializer
 
 
-class ArticleView(APIView):
+class ArticleView(ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
 
-    def get(self, request):
-        articles = Article.objects.all()
-        # the many param informs the serializer that it will be serializing more than a single article.
-        serializer = ArticleSerializer(articles, many=True)
-        return Response({"articles": serializer.data})
+    def perform_create(self, serializer):
+        author = get_object_or_404(
+            Author, id=self.request.data.get('author_id'))
+        return serializer.save(author=author)
 
-    def get(self, request, pk):
-        saved_article = get_object_or_404(Article.objects.all(), pk=pk)
-        # the many param informs the serializer that it will be serializing more than a single article.
-        serializer = ArticleSerializer(saved_article, many=True)
-        return Response({"article": serializer.data})
 
-    def post(self, request):
-        article = request.data.get('article')
-        # Create an article from the above data
-        serializer = ArticleSerializer(data=article)
-        if serializer.is_valid(raise_exception=True):
-            article_saved = serializer.save()
-        return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
-
-    def put(self, request, pk):
-        saved_article = get_object_or_404(Article.objects.all(), pk=pk)
-        data = request.data.get('article')
-        serializer = ArticleSerializer(
-            instance=saved_article, data=data, partial=True)
-        if serializer.is_valid(raise_exception=True):
-            article_saved = serializer.save()
-        return Response({
-            "success": "Article '{}' updated successfully".format(article_saved.title)
-        })
-
-    def delete(self, request, pk):
-        # Get object with this pk
-        article = get_object_or_404(Article.objects.all(), pk=pk)
-        article.delete()
-        return Response({
-            "message": "Article with id `{}` has been deleted.".format(pk)
-        }, status=204)
+class SingleArticleView(RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
